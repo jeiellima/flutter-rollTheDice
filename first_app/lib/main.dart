46197864c7beb1685d731b6df95e28c23cd0bb24
Jeiel Lima <112661437+jeiellima@users.noreply.github.com>
2024-06-05 18:17:25 -0300

    add: adicionando color

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -3,7 +3,8 @@ import "package:flutter/material.dart";
 void main() {
   runApp(
     const MaterialApp(
-      home: Scaffold(
+      home: Scaffold( 
+        backgroundColor: Color.fromARGB(255, 92, 57, 156) ,
         body: Center(
           child: Text('Tu vai conquistar o mundo!'),
         ),
