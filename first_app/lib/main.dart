a89b44ebe00ccf2ce73c6c4ea3c9f8393db94e4a
Jeiel Lima <112661437+jeiellima@users.noreply.github.com>
2024-06-07 17:32:34 -0300

    add: adicionando classe

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -2,13 +2,33 @@ import "package:flutter/material.dart";
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Scaffold( 
-        backgroundColor: Color.fromARGB(255, 92, 57, 156) ,
-        body: Center(
-          child: Text('Tu vai conquistar o mundo!'),
-        ),
+    MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Color.fromARGB(31, 52, 14, 124),
+          Color.fromARGB(255, 70, 29, 122),
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            backgroundColor: Colors.white,
+            fontSize: 25,
+          ),
+        ),
+      ),
+    );
+  }
 }
