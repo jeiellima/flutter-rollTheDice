137ed1dd4984ee49e9a587ae5636b42db079d3b0
Jeiel Lima <112661437+jeiellima@users.noreply.github.com>
2024-06-19 17:19:17 -0300

    add: adicionando assets + novas funções

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,34 +1,12 @@
 import "package:flutter/material.dart";
+import 'package:first_app/grandient_container.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: GradientContainer(),
+        body: GradientContainer(Colors.deepPurple, Colors.lightBlue),
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Color.fromARGB(31, 52, 14, 124),
-          Color.fromARGB(255, 70, 29, 122),
-        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            backgroundColor: Colors.white,
-            fontSize: 25,
-          ),
-        ),
-      ),
-    );
-  }
 }
